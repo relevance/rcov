f97caf3f4362a29268966d90b24ca66cb688eebf
Mauricio Fernandez <mfp@acm.org>
2006-05-24 06:22:05 -0400

    lib/rcov/lowlevel.rb, bin/rcov: implemented --no-rcovrt, which sets $rcov_do_not_use_rcovrt.
    darcs-hash:20060524102205-0786a-7d4ba8ee49cdda1f94e662ac2628e9573f794cc6.gz

--- lib/rcov/lowlevel.rb
+++ lib/rcov/lowlevel.rb
@@ -17,24 +17,27 @@ module Rcov
   # stable interface.
 module RCOV__
   COVER = {}
-  begin
-    require 'rcovrt'
-    abi = [0,0,0]
+  pure_ruby_impl_needed = true
+  unless defined? $rcov_do_not_use_rcovrt 
     begin
-      abi = RCOV__.ABI
-      raise if abi[0] != RCOVRT_ABI[0] || abi[1] < RCOVRT_ABI[1]
-    rescue
-      $stderr.puts <<-EOF
+      require 'rcovrt'
+      abi = [0,0,0]
+      begin
+        abi = RCOV__.ABI
+        raise if abi[0] != RCOVRT_ABI[0] || abi[1] < RCOVRT_ABI[1]
+        pure_ruby_impl_needed = false
+      rescue
+        $stderr.puts <<-EOF
 The rcovrt extension I found was built for a different version of rcov.
 The required ABI is:              #{RCOVRT_ABI.join(".")}
 Your current rcovrt extension is: #{abi.join(".")}
 
 Please delete rcovrt.{so,bundle,dll,...} and install the required one.
-      EOF
-      raise LoadError
-    end
-  rescue LoadError
-    $stderr.puts <<-EOF
+        EOF
+        raise LoadError
+      end
+    rescue LoadError
+      $stderr.puts <<-EOF
 
 Since the rcovrt extension couldn't be loaded, rcov will run in pure-Ruby
 mode, which is about two orders of magnitude slower.
@@ -42,8 +45,11 @@ mode, which is about two orders of magnitude slower.
 If you're on win32, you can find a pre-built extension (usable with recent
 One Click Installer and mswin32 builds) at http://eigenclass.org/hiki.rb?rcov .
 
-    EOF
+      EOF
+    end
+  end
 
+  if pure_ruby_impl_needed
     methods = %w[install_hook remove_hook reset generate_coverage_info]
     sklass = class << self; self end
     (methods & sklass.instance_methods).each do |meth|
