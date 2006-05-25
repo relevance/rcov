12e86dcb8482aaa52744cfdefa52ca40a57ef2a1
Mauricio Fernandez <mfp@acm.org>
2006-05-25 14:58:49 -0400

    ext/rcovrt/rcov.c: implemented RCOV__.generate_tracer_info.
    darcs-hash:20060525185849-0786a-faf513ef9a00aede384e93379679a47c2d378d5f.gz

--- ext/rcovrt/rcov.c
+++ ext/rcovrt/rcov.c
@@ -5,13 +5,14 @@
 #include <stdlib.h>
 
 #define RCOVRT_VERSION_MAJOR 0
-#define RCOVRT_VERSION_MINOR 1
-#define RCOVRT_VERSION_REV   1
+#define RCOVRT_VERSION_MINOR 2
+#define RCOVRT_VERSION_REV   0
 
 static VALUE mRcov;
 static VALUE mRCOV__;
 static VALUE oSCRIPT_LINES__;
 static ID id_cover;
+static VALUE id_caller = 0;
 static st_table* coverinfo = 0;
 static char hook_set_p;
 
@@ -23,6 +24,36 @@ struct cov_array {
 static struct cov_array *cached_array = 0;
 static char *cached_file = 0; 
 
+static VALUE caller_info = 0;
+
+static caller_stack_len = 1;
+
+static VALUE
+record_call_trace_info(VALUE args)
+{
+  VALUE caller_ary;
+  VALUE curr_meth;
+  VALUE count_hash;
+  VALUE count;
+  VALUE *pargs = (VALUE *)args;
+
+  caller_ary = pargs[0];
+  curr_meth = pargs[1];
+  count_hash = rb_hash_aref(caller_info, curr_meth);
+  if(TYPE(count_hash) != T_HASH) { 
+          /* Qnil, anything else should be impossible */
+          count_hash = rb_hash_new();
+          rb_hash_aset(caller_info, curr_meth, count_hash);
+  }
+  count = rb_hash_aref(count_hash, caller_ary);
+  if(count == Qnil) 
+          count = INT2FIX(0);
+  count = INT2FIX(FIX2UINT(count) + 1);
+  rb_hash_aset(count_hash, caller_ary, count);
+
+  return Qnil;
+}
+
 static void
 coverage_event_hook(rb_event_t event, NODE *node, VALUE self, 
                 ID mid, VALUE klass)
@@ -30,6 +61,26 @@ coverage_event_hook(rb_event_t event, NODE *node, VALUE self,
  char *sourcefile;
  unsigned int sourceline;
  
+ if(event == RUBY_EVENT_CALL) {
+         VALUE caller_ary;
+         VALUE aref_args[2];
+         VALUE curr_meth;
+         VALUE args[2];
+
+         caller_ary = rb_funcall(rb_mKernel, id_caller, 0);
+         aref_args[0] = INT2FIX(0);
+         aref_args[1] = INT2NUM(caller_stack_len);
+         caller_ary = rb_ary_aref(2, aref_args, caller_ary);
+
+         curr_meth = rb_ary_new();
+         rb_ary_push(curr_meth, klass);
+         rb_ary_push(curr_meth, ID2SYM(mid));
+
+         args[0] = caller_ary;
+         args[1] = curr_meth;
+         rb_protect(record_call_trace_info, (VALUE)args, 0);
+ }
+
  if(event & (RUBY_EVENT_C_CALL | RUBY_EVENT_C_RETURN | RUBY_EVENT_CLASS))
          return;
  
@@ -149,6 +200,11 @@ cov_generate_coverage_info(VALUE self)
   return cover;
 }
 
+static VALUE
+cov_generate_tracer_info(VALUE self)
+{
+  return caller_info;
+}
 
 static VALUE
 cov_reset(VALUE self)
@@ -164,6 +220,8 @@ cov_reset(VALUE self)
   st_foreach(coverinfo, free_table, Qnil); 
   st_free_table(coverinfo);
   coverinfo = 0;
+
+  caller_info = rb_hash_new();
   return Qnil;
 }
 
@@ -188,6 +246,7 @@ Init_rcovrt()
  ID id_script_lines__ = rb_intern("SCRIPT_LINES__");
  
  id_cover = rb_intern("COVER");
+ id_caller = rb_intern("caller");
 
  if(rb_const_defined(rb_cObject, id_rcov)) 
          mRcov = rb_const_get(rb_cObject, id_rcov);
@@ -206,12 +265,18 @@ Init_rcovrt()
          rb_const_set(rb_cObject, id_script_lines__, oSCRIPT_LINES__);
  }
 
+ caller_info = rb_hash_new();
+ rb_gc_register_address(&caller_info);
+
  hook_set_p = 0;
 
  rb_define_singleton_method(mRCOV__, "install_hook", cov_install_hook, 0);
  rb_define_singleton_method(mRCOV__, "remove_hook", cov_remove_hook, 0);
  rb_define_singleton_method(mRCOV__, "generate_coverage_info", 
 		 cov_generate_coverage_info, 0);
+ rb_define_singleton_method(mRCOV__, "generate_tracer_info", 
+		 cov_generate_tracer_info, 0);
  rb_define_singleton_method(mRCOV__, "reset", cov_reset, 0);
  rb_define_singleton_method(mRCOV__, "ABI", cov_ABI, 0);
 }
+/* vim: set sw=8 expandtab: */
