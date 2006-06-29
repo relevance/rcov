b91a5a78090942eda460e0806d9203f1ee45cd3c
Mauricio Fernandez <mfp@acm.org>
2006-06-29 05:28:39 -0400

    lib/rcov/report.rb (Formatter): normalize pathnames in default ignore list.
    darcs-hash:20060629092839-0786a-c59540111b6d53f10b2252744e91a919be913a30.gz

--- lib/rcov/report.rb
+++ lib/rcov/report.rb
@@ -4,8 +4,14 @@
 module Rcov
 
 class Formatter # :nodoc:
-    ignore_files = [/\A#{Regexp.escape(Config::CONFIG["libdir"])}/, /\btc_[^.]*.rb/,
-        /_test\.rb\z/, /\btest\//, /\bvendor\//, /\A#{Regexp.escape(__FILE__)}\z/]
+    require 'pathname'
+    ignore_files = [
+        /\A#{Regexp.escape(Pathname.new(Config::CONFIG["libdir"]).cleanpath.to_s)}/,
+        /\btc_[^.]*.rb/,
+        /_test\.rb\z/,
+        /\btest\//,
+        /\bvendor\//,
+        /\A#{Regexp.escape(__FILE__)}\z/]
     DEFAULT_OPTS = {:ignore => ignore_files, :sort => :name, :sort_reverse => false,
                     :output_threshold => 101, :dont_ignore => [], 
                     :callsite_analyzer => nil, :comments_run_by_default => false}
