148524e3829d64756495b155dde7b3873a363b71
Mauricio Fernandez <mfp@acm.org>
2006-08-09 05:58:44 -0400

    lib/rcov/version.rb: bumped VERSION to 0.7.1 unreleased.
    darcs-hash:20060809095844-0786a-bb32542d36d5d5e8a8fd2dca6d486bbd093a4bac.gz

--- lib/rcov/version.rb
+++ lib/rcov/version.rb
@@ -4,8 +4,8 @@
 
 module Rcov
 
-VERSION = "0.7.0"
-RELEASE_DATE = "2006/08/04"
+VERSION = "0.7.1"
+RELEASE_DATE = "unreleased (internal/preliminary)"
 RCOVRT_ABI = [2,0,0]
 UPSTREAM_URL = "http://eigenclass.org/hiki.rb?rcov"
 
