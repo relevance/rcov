67b477bfbcc0384940b0fc6871198ac03d689665
rubikitch <rubikitch@ruby-lang.org>
2006-12-16 10:19:08 -0500

    functional test for --save/-D
    darcs-hash:20061216151908-b2eea-fe44d7e1968fb4ffae772a4a86fe932c97101c38.gz

--- test/test_functional.rb
+++ test/test_functional.rb
@@ -35,12 +35,16 @@ class TestFunctional < Test::Unit::TestCase
     assert_equal(content["expected_coverage"], content["actual_coverage"])
   end
 
-  def run_rcov(opts, opts_tail="")
+  def with_testdir(&block)
+    Dir.chdir(@@dir, &block)
+  end
+
+  def run_rcov(opts, script="sample_04.rb", opts_tail="")
     rcov = @@dir+"../bin/rcov"
     ruby_opts = "-I../lib:../ext/rcovrt"
-    Dir.chdir(@@dir) do
-      `cd #{@@dir}; ruby #{ruby_opts} #{rcov} #{opts} -o actual_coverage sample_04.rb #{opts_tail}`
-      yield
+    with_testdir do
+      `cd #{@@dir}; ruby #{ruby_opts} #{rcov} #{opts} -o actual_coverage #{script} #{opts_tail}`
+      yield if block_given?
     end
   end
 
@@ -58,11 +62,44 @@ class TestFunctional < Test::Unit::TestCase
     end
   end
 
+  @@selection = "--include-file=sample --exclude=rcov"
   def test_text_gcc
-    run_rcov("--gcc --include-file=sample --exclude=rcov",
+    run_rcov("--gcc #{@@selection}",
+             "sample_04.rb",
              "> actual_coverage/gcc-text.out") do
       cmp "gcc-text.out"
     end
+  end
+
+  def test_diff
+    with_testdir { FileUtils.cp "sample_05-old.rb", "sample_05.rb" }
+    run_rcov("--no-html --gcc #{@@selection} --save=coverage.info", "sample_05.rb",
+             "> actual_coverage/diff-gcc-original.out") do
+      cmp "diff-gcc-original.out"
+    end
+
+    with_testdir { FileUtils.cp "sample_05-new.rb", "sample_05.rb" }
+    run_rcov("--no-html -D --gcc #{@@selection}", "sample_05.rb",
+             "> actual_coverage/diff-gcc-diff.out") do
+      cmp "diff-gcc-diff.out"
+    end
+
+    run_rcov("--no-html -D #{@@selection}", "sample_05.rb",
+             "> actual_coverage/diff.out") do
+      cmp "diff.out"
+    end
+
+    run_rcov("--no-html --no-color -D #{@@selection}", "sample_05.rb",
+             "> actual_coverage/diff-no-color.out") do
+      cmp "diff-no-color.out"
+    end
+
+    run_rcov("--no-html --gcc #{@@selection}", "sample_05.rb",
+             "> actual_coverage/diff-gcc-all.out") do
+      cmp "diff-gcc-all.out"
+    end
+
+    
   end
 
 end
