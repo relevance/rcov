23cebe1a4ba709ad92519d8a1c9efaff0da83238
Chad Humphries <chad@spicycode.com>
2008-10-29 21:37:28 -0400

    Updating gemspec version

--- lib/rcov/version.rb
+++ lib/rcov/version.rb
@@ -4,10 +4,10 @@
 
 module Rcov
 
-VERSION = "0.8.1.3"
-RELEASE_DATE = "2008-08-29"
+VERSION = "0.8.1.5"
+RELEASE_DATE = "2008-10-29"
 RCOVRT_ABI = [2,0,0]
-UPSTREAM_URL = "http://eigenclass.org/hiki/rcov"
+UPSTREAM_URL = "http://github.com/spicycode/rcov"
 
 end
 # vi: set sw=2:
