bca4f62782d21390162d6b8b8f2fd722f69b67c6
Rob Sanheim <rsanheim@gmail.com>
2009-02-11 23:22:57 -0500

    string needs to be longer then the wrap width, and viola, we have exposed the bug

--- test/rexml_test.rb
+++ test/rexml_test.rb
@@ -5,7 +5,8 @@ require 'rexml/formatters/pretty'
 class TestRexml < Test::Unit::TestCase
   def test_bug_with_long_lines_without_spaces
     pretty_formatter = ::REXML::Formatters::Pretty.new
-    long_string = "this-is-a-long-string-without-any-spaces-to-try-to-break-rexml-formatter"
+    long_string = "this-is-a-long-string-without-any-spaces-to-try-to-break-rexml-formatter-and-it-is-over-100-characters-long"
     pretty_formatter.send(:wrap, long_string, 100)
+
   end
 end
