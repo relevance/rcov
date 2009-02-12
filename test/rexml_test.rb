b4084eb0bc1c2e7ec5f3a3ae8e2ec0456bc2cc05
Rob Sanheim <rsanheim@gmail.com>
2009-02-11 23:21:12 -0500

    trying to expose rexml formatter bug that often breaks Rcov with Ruby 1.8.6

---
+++ test/rexml_test.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/test_helper'
+require 'rexml/document'
+require 'rexml/formatters/pretty'
+
+class TestRexml < Test::Unit::TestCase
+  def test_bug_with_long_lines_without_spaces
+    pretty_formatter = ::REXML::Formatters::Pretty.new
+    long_string = "this-is-a-long-string-without-any-spaces-to-try-to-break-rexml-formatter"
+    pretty_formatter.send(:wrap, long_string, 100)
+  end
+end
