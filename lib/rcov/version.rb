7ab8b824d50e326af75325f335bdab68182d8179
Chad Humphries <chad@spicycode.com>
2009-05-08 15:48:25 -0400

    0.8.3.1 - CSS Fixes

--- lib/rcov/version.rb
+++ lib/rcov/version.rb
@@ -3,7 +3,7 @@
 # See LEGAL and LICENSE for licensing information.
 
 module Rcov
-  VERSION = "0.8.3.0"
+  VERSION = "0.8.3.1"
   RELEASE_DATE = "2009-05-08"
   RCOVRT_ABI = [2,0,0]
   UPSTREAM_URL = "http://github.com/relevance/rcov"
