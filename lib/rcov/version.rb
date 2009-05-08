7352b133b29394dae2c45080bba389eb6a5fe828
Rob Sanheim <rsanheim@gmail.com>
2009-05-08 09:48:22 -0400

    one more find replace

--- lib/rcov/version.rb
+++ lib/rcov/version.rb
@@ -6,6 +6,6 @@ module Rcov
   VERSION = "0.8.2.1"
   RELEASE_DATE = "2009-03-17"
   RCOVRT_ABI = [2,0,0]
-  UPSTREAM_URL = "http://github.com/spicycode/rcov"
+  UPSTREAM_URL = "http://github.com/relevance/rcov"
 
 end
