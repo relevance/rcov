66a0becfd21f48950bdde1632371ee022cbc68c6
Aaron Bedra <aaron@thinkrelevance.com>
2009-07-27 14:29:04 -0400

    really fix the mkmf requirement stuff and bump version

--- lib/rcov/formatters/base_formatter.rb
+++ lib/rcov/formatters/base_formatter.rb
@@ -2,7 +2,7 @@ module Rcov
   
   class BaseFormatter # :nodoc:
     require 'pathname'
-    ignore_files = [/\A#{Regexp.escape(Pathname.new(::Config::CONFIG['libdir']).cleanpath.to_s)}/, /\btc_[^.]*.rb/, /_test\.rb\z/, /\btest\//, /\bvendor\//, /\A#{Regexp.escape(__FILE__)}\z/]
+    ignore_files = []
 
     DEFAULT_OPTS = {:ignore => ignore_files, :sort => :name, :sort_reverse => false,
       :output_threshold => 101, :dont_ignore => [], :callsite_analyzer => nil, :comments_run_by_default => false}
