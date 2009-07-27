5e8463727af4fb66f29d605fea8d3b528ed1e995
Aaron Bedra <aaron@thinkrelevance.com>
2009-07-27 15:13:38 -0400

    replacing ignore list and mkmf requirement because it adds noise to the coverage results.

--- lib/rcov/version.rb
+++ lib/rcov/version.rb
@@ -3,8 +3,8 @@
 # See LEGAL and LICENSE for licensing information.
 
 module Rcov
-  VERSION = "0.8.3.7"
-  RELEASE_DATE = "2009-06-2"
+  VERSION = "0.8.3.8"
+  RELEASE_DATE = "2009-07-27"
   RCOVRT_ABI = [2,0,0]
   UPSTREAM_URL = "http://github.com/relevance/rcov"
 end
