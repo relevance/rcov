132189c0d4a25032fd94e88a65137c3c50fdae38
Aaron Bedra / Corey Ehmke <opensource@thinkrelevance.com>
2009-08-30 14:25:40 -0400

    bumping version after ui enhancements

--- lib/rcov/version.rb
+++ lib/rcov/version.rb
@@ -3,8 +3,8 @@
 # See LICENSE for licensing information.
 
 module Rcov
-  VERSION = "0.8.7"
-  RELEASE_DATE = "2009-08-10"
+  VERSION = "0.9.0"
+  RELEASE_DATE = "2009-08-30"
   RCOVRT_ABI = [2,0,0]
   UPSTREAM_URL = "http://github.com/relevance/rcov"
 end
