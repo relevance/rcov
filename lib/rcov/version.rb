c52b94ae43038b0991d671a4c3249f5be9578236
Aaron Bedra / Jay McGaffigan <opensource@thinkrelevance.com>
2009-09-11 14:45:01 -0400

    bumping version

--- lib/rcov/version.rb
+++ lib/rcov/version.rb
@@ -3,7 +3,7 @@
 # See LICENSE for licensing information.
 
 module Rcov
-  VERSION = "0.9.0"
+  VERSION = "0.9.1"
   RELEASE_DATE = "2009-08-30"
   RCOVRT_ABI = [2,0,0]
   UPSTREAM_URL = "http://github.com/relevance/rcov"
