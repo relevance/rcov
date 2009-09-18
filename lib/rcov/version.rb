276e311a81aaece6753c345645f561aca490364f
Aaron Bedra <aaron@aaronbedra.com>
2009-09-18 10:26:17 -0400

    bumping version

--- lib/rcov/version.rb
+++ lib/rcov/version.rb
@@ -3,7 +3,7 @@
 # See LICENSE for licensing information.
 
 module Rcov
-  VERSION = "0.9.2"
+  VERSION = "0.9.2.1"
   RELEASE_DATE = "2009-09-18"
   RCOVRT_ABI = [2,0,0]
   UPSTREAM_URL = "http://github.com/relevance/rcov"
