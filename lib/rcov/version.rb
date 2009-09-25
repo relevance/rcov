0d4ae40cd184ba7cc21ad2163b644945aa0682e0
Aaron Bedra <aaron@aaronbedra.com>
2009-09-25 11:15:02 -0400

    fixing aggregate_data bug in 1.9 thanks to @dguettler

--- lib/rcov/version.rb
+++ lib/rcov/version.rb
@@ -3,8 +3,8 @@
 # See LICENSE for licensing information.
 
 module Rcov
-  VERSION = "0.9.2.1"
-  RELEASE_DATE = "2009-09-18"
+  VERSION = "0.9.3"
+  RELEASE_DATE = "2009-09-25"
   RCOVRT_ABI = [2,0,0]
   UPSTREAM_URL = "http://github.com/relevance/rcov"
 end
