b01d161f6cab16772891bd1eea01c7df44d8b0af
John Feminella <johnf@bitsbuilder.com>
2010-09-22 20:27:59 +0000

    Correct misspelling/poor phrasing on failure reports

--- lib/rcov/formatters/failure_report.rb
+++ lib/rcov/formatters/failure_report.rb
@@ -4,11 +4,11 @@ module Rcov
       puts summary
       coverage = code_coverage * 100
       if coverage < @failure_threshold
-        puts "You failed to satisfy the coverage theshold of #{@failure_threshold}%"
+        puts "You failed to satisfy the coverage threshold of #{@failure_threshold}%."
         exit(1)
       end
       if (coverage - @failure_threshold) > 3
-        puts "Your coverage has significantly increased over your threshold of #{@failure_threshold}. Please increase it."
+        puts "Your coverage is close to your threshold of #{@failure_threshold}. Please increase it."
       end
     end
   end
