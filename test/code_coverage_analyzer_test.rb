4ee14bb1412331ca70a8e77a4525089cadbd5286
Aaron Bedra <aaron@aaronbedra.com>
2012-01-17 10:13:37 -0600

    Looking at code coverage analyzer. A little reformatting and cleanup before 1.8.7 fixes

--- test/code_coverage_analyzer_test.rb
+++ test/code_coverage_analyzer_test.rb
@@ -24,8 +24,8 @@ EOF
   def test_refine_coverage_info
     analyzer = Rcov::CodeCoverageAnalyzer.new
     cover = [1, 1, nil, nil, 0, 5, 5, 5, 0]
-    line_info, marked_info,
-      count_info = analyzer.instance_eval{ refine_coverage_info(LINES, cover) }
+
+    line_info, marked_info, count_info = analyzer.instance_eval{ refine_coverage_info(LINES, cover) }
     assert_equal(LINES, line_info)
     assert_equal([true] * 2 + [false] * 3 + [true] * 3 + [false], marked_info)
     assert_equal([1, 1, 0, 0, 0, 5, 5, 5, 0], count_info)
@@ -39,6 +39,7 @@ EOF
   def test_raw_coverage_info
     sample_file = File.join(File.dirname(__FILE__), "assets/sample_01.rb")
     lines = File.readlines(sample_file)
+
     analyzer = Rcov::CodeCoverageAnalyzer.new
     analyzer.run_hooked{ load sample_file }
 
@@ -48,8 +49,12 @@ EOF
     assert_equal(lines, line_info)
     assert_equal([true, true, false, false, true, false, true], cov_info)
     assert_equal([1, 2, 0, 0, 1, 0, 11], count_info) unless RUBY_PLATFORM =~ /java/
-    # JRUBY reports an if x==blah as hitting this type of line once, JRUBY also optimizes this stuff so you'd have to run with --debug to get "extra" information.  MRI hits it twice.
+
+    # JRUBY reports an if x==blah as hitting this type of line once,
+    # JRUBY also optimizes this stuff so you'd have to run with
+    # --debug to get "extra" information. MRI hits it twice.
     assert_equal([1, 3, 0, 0, 1, 0, 13], count_info) if RUBY_PLATFORM =~ /java/
+
     analyzer.reset
     assert_equal(nil, analyzer.data(sample_file))
     assert_equal([], analyzer.analyzed_files)
@@ -60,6 +65,7 @@ EOF
     lines = ["puts a", "foo", "bar"] * 3
     coverage = [true] * 3 + [false] * 6
     counts = [1] * 3 + [0] * 6
+
     nlines, ncoverage, ncounts = analyzer.instance_eval do
       script_lines_workaround(lines, coverage, counts)
     end
@@ -74,9 +80,11 @@ EOF
     lines = ["puts a", "foo", "bar"] * 2 + ["puts a", "foo", "baz"]
     coverage = [true] * 9
     counts = [1] * 9
+
     nlines, ncoverage, ncounts = analyzer.instance_eval do
       script_lines_workaround(lines, coverage, counts)
     end
+
     assert_equal(lines, nlines)
     assert_equal(coverage, ncoverage)
     assert_equal(counts, ncounts)
@@ -85,10 +93,13 @@ EOF
   def test_if_elsif_reports_correctly
     sample_file = File.join(File.dirname(__FILE__), "assets/sample_06.rb")
     lines = File.readlines(sample_file)
+
     analyzer = Rcov::CodeCoverageAnalyzer.new
     analyzer.run_hooked{ load sample_file }
+
     assert_equal(lines, SCRIPT_LINES__[sample_file][0, lines.size])
     assert(analyzer.analyzed_files.include?(sample_file))
+
     line_info, cov_info, count_info = analyzer.data(sample_file)
     assert_equal(lines, line_info)
     assert_equal([true, true, false, true, true, false, false, false], cov_info) unless RUBY_PLATFORM == "java"
@@ -97,46 +108,49 @@ EOF
   def test_differential_coverage_data
     sample_file = File.join(File.dirname(__FILE__), "assets/sample_01.rb")
     lines = File.readlines(sample_file)
+
     analyzer = Rcov::CodeCoverageAnalyzer.new
     analyzer.run_hooked{ load sample_file }
     line_info, cov_info, count_info = analyzer.data(sample_file)
-    assert_equal([1, 2, 0, 0, 1, 0, 11], count_info) if RUBY_VERSION =~ /1.9/
 
     analyzer.reset
-    #set_trace_func proc { |event, file, line, id, binding, classname| printf "%8s %s:%-2d %10s %8s\n", event, file, line, id, classname if (file =~ /sample_02.rb/) }     
 
     sample_file = File.join(File.dirname(__FILE__), "assets/sample_02.rb")
     analyzer.run_hooked{ load sample_file }
     line_info, cov_info, count_info = analyzer.data(sample_file)
+
     if RUBY_PLATFORM == "java"
       assert_equal([8, 3, 0, 0, 0], count_info) 
     else
-      assert_equal([8, 1, 0, 0, 0], count_info) unless  RUBY_VERSION =~ /1.9/
-      assert_equal([4, 1, 0, 0, 4], count_info) if RUBY_VERSION =~ /1.9/
+      assert_equal([8, 1, 0, 0, 0], count_info)
     end
 
     analyzer.reset
     assert_equal([], analyzer.analyzed_files)
     analyzer.run_hooked{ Rcov::Test::Temporary::Sample02.foo(1, 1) }
     line_info, cov_info, count_info = analyzer.data(sample_file)
+
     if RUBY_PLATFORM == "java"
-      assert_equal([0, 1, 3, 1, 0], count_info) unless RUBY_VERSION =~ /1.9/
+      assert_equal([0, 1, 3, 1, 0], count_info)
     else
-      assert_equal([0, 1, 1, 1, 0], count_info) unless RUBY_VERSION =~ /1.9/
-      assert_equal([0, 2, 1, 0, 0], count_info) if RUBY_VERSION =~ /1.9/
+      assert_equal([0, 1, 1, 1, 0], count_info)
     end
+
     analyzer.run_hooked do
       10.times{ Rcov::Test::Temporary::Sample02.foo(1, 1) }
     end
+
     line_info, cov_info, count_info = analyzer.data(sample_file)
     assert_equal([0, 11, 33, 11, 0], count_info) if RUBY_PLATFORM == "java"
     assert_equal([0, 11, 11, 11, 0], count_info) unless RUBY_PLATFORM == "java"
+
     10.times{ analyzer.run_hooked{ Rcov::Test::Temporary::Sample02.foo(1, 1) } }
     line_info, cov_info, count_info = analyzer.data(sample_file)
     assert_equal([0, 21, 63, 21, 0], count_info) if RUBY_PLATFORM == "java"
     assert_equal([0, 21, 21, 21, 0], count_info) unless RUBY_PLATFORM == "java"
 
     count_info2 = nil
+
     10.times do |i|
       analyzer.run_hooked do
         Rcov::Test::Temporary::Sample02.foo(1, 1)
@@ -144,6 +158,7 @@ EOF
         line_info2, cov_info2, count_info2 = analyzer.data(sample_file)
       end
     end
+
     if RUBY_PLATFORM == "java"
       assert_equal([0, 25, 75, 25, 0], count_info)
       assert_equal([0, 31, 93, 31, 0], count_info2)
@@ -162,9 +177,11 @@ EOF
 
     a1.run_hooked do
       100.times{ Rcov::Test::Temporary::Sample02.foo(1, 1) }
+
       a2.run_hooked do
         10.times{ Rcov::Test::Temporary::Sample02.foo(1, 1) }
       end
+
       100.times{ Rcov::Test::Temporary::Sample02.foo(1, 1) }
     end
 
@@ -183,6 +200,7 @@ EOF
 
     _, _, counts1 = a1.data(sample_file)
     _, _, counts2 = a2.data(sample_file)
+
     if RUBY_PLATFORM == "java"
       assert_equal([0, 221, 663, 221, 0], counts1)    
       assert_equal([0, 121, 363, 121, 0], counts2)
@@ -214,7 +232,7 @@ EOF
     first = {"a" => [1,1,1,1,1]}
     last =  {"a" => [2,1,5,2,1], "b" => [1,2,3,4,5]}
     a = Rcov::CodeCoverageAnalyzer.new
-    assert_equal({"a" => [1,0,4,1,0], "b" => [1,2,3,4,5]},
-                 a.instance_eval{ compute_raw_data_difference(first, last)} )
+
+    assert_equal({"a" => [1,0,4,1,0], "b" => [1,2,3,4,5]}, a.instance_eval{ compute_raw_data_difference(first, last)} )
   end
 end
