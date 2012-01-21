dea676c0b45f6d09b5ad99d3b3d6069018d35728
Aaron Bedra <aaron@aaronbedra.com>
2012-01-20 23:48:46 -0600

    Major cleanup. Temporarily breaking JRuby support

--- lib/rcov/version.rb
+++ lib/rcov/version.rb
@@ -1,10 +1,6 @@
-# rcov Copyright (c) 2004-2006 Mauricio Fernandez <mfp@acm.org>
-#
-# See LICENSE for licensing information.
-
 module Rcov
-  VERSION = "0.9.11"
-  RELEASE_DATE = "2010-02-28"
+  VERSION = "1.0.0"
+  RELEASE_DATE = "2012-02-01"
   RCOVRT_ABI = [2,0,0]
   UPSTREAM_URL = "http://github.com/relevance/rcov"
 end
